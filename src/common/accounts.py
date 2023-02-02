f262a26f36c0afb9d25ceee9ae3c759d7a5fc238
cyc60 <avsysoev60@gmail.com>
2023-02-02 17:30:11 +0300

    Rename wallet to operator
    Signed-off-by: cyc60 <avsysoev60@gmail.com>

--- src/common/accounts.py
+++ src/common/accounts.py
@@ -5,33 +5,33 @@ from eth_account import Account
 from eth_account.account import LocalAccount
 
 from src.config.settings import (
-    WALLET_KEYSTORE_PASSWORD_PATH,
-    WALLET_KEYSTORE_PATH,
-    WALLET_PRIVATE_KEY,
+    OPERATOR_KEYSTORE_PASSWORD_PATH,
+    OPERATOR_KEYSTORE_PATH,
+    OPERATOR_PRIVATE_KEY,
 )
 
 
 def get_operator_account() -> LocalAccount:
-    if WALLET_PRIVATE_KEY:
-        return Account().from_key(WALLET_PRIVATE_KEY)
+    if OPERATOR_PRIVATE_KEY:
+        return Account().from_key(OPERATOR_PRIVATE_KEY)
 
-    if WALLET_KEYSTORE_PATH and WALLET_KEYSTORE_PASSWORD_PATH:
-        if not os.path.isfile(WALLET_KEYSTORE_PATH):
-            raise ValueError(f"Can't open WALLET_KEYSTORE_PATH file. "
-                             f' Path: {WALLET_KEYSTORE_PATH}')
-        if not os.path.isfile(WALLET_KEYSTORE_PASSWORD_PATH):
-            raise ValueError(f"Can't open WALLET_KEYSTORE_PASSWORD_PATH file. "
-                             f'Path: {WALLET_KEYSTORE_PASSWORD_PATH}')
+    if OPERATOR_KEYSTORE_PATH and OPERATOR_KEYSTORE_PASSWORD_PATH:
+        if not os.path.isfile(OPERATOR_KEYSTORE_PATH):
+            raise ValueError(f"Can't open OPERATOR_KEYSTORE_PATH file. "
+                             f' Path: {OPERATOR_KEYSTORE_PATH}')
+        if not os.path.isfile(OPERATOR_KEYSTORE_PASSWORD_PATH):
+            raise ValueError(f"Can't open OPERATOR_KEYSTORE_PASSWORD_PATH file. "
+                             f'Path: {OPERATOR_KEYSTORE_PASSWORD_PATH}')
 
-        with open(WALLET_KEYSTORE_PATH, 'r', encoding='utf-8') as f:
+        with open(OPERATOR_KEYSTORE_PATH, 'r', encoding='utf-8') as f:
             keyfile_json = json.load(f)
-        with open(WALLET_KEYSTORE_PASSWORD_PATH, 'r', encoding='utf-8') as f:
+        with open(OPERATOR_KEYSTORE_PASSWORD_PATH, 'r', encoding='utf-8') as f:
             password = f.read().strip()
         key = Account().decrypt(keyfile_json, password)
         return Account().from_key(key)
 
-    raise ValueError('Provide WALLET_PRIVATE_KEY setting or combination of '
-                     'WALLET_KEYSTORE_PATH and WALLET_KEYSTORE_PASSWORD_PATH settings')
+    raise ValueError('Provide OPERATOR_PRIVATE_KEY setting or combination of '
+                     'OPERATOR_KEYSTORE_PATH and OPERATOR_KEYSTORE_PASSWORD_PATH settings')
 
 
 operator_account = get_operator_account()
