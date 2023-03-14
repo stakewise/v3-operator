4b8a07b3c383f5b93d9cca5eb72601c096fdb428
Evgeny Gusarov <evgeny@stakewise.io>
2023-03-14 12:10:48 +0300

    Add retries to ipfs client

--- src/common/clients.py
+++ src/common/clients.py
@@ -7,12 +7,14 @@ from sw_utils import (
     get_consensus_client,
     get_execution_client,
 )
+from sw_utils.decorators import backoff_requests_errors
 from web3 import Web3
 
 from src.common.accounts import operator_account
 from src.config.settings import (
     CONSENSUS_ENDPOINT,
     DATABASE,
+    DEFAULT_RETRY_TIME,
     EXECUTION_ENDPOINT,
     IPFS_FETCH_ENDPOINTS,
 )
@@ -33,7 +35,17 @@ def build_execution_client() -> Web3:
     return w3
 
 
+class IpfsFetchRetryClient(IpfsFetchClient):
+    @backoff_requests_errors(max_time=DEFAULT_RETRY_TIME)
+    async def fetch_bytes(self, ipfs_hash: str) -> bytes:
+        return await super().fetch_bytes(ipfs_hash)
+
+    @backoff_requests_errors(max_time=DEFAULT_RETRY_TIME)
+    async def fetch_json(self, ipfs_hash: str) -> dict | list:
+        return await super().fetch_json(ipfs_hash)
+
+
 execution_client = build_execution_client()
 consensus_client = get_consensus_client(CONSENSUS_ENDPOINT)
 db_client = Database()
-ipfs_fetch_client = IpfsFetchClient(IPFS_FETCH_ENDPOINTS)
+ipfs_fetch_client = IpfsFetchRetryClient(IPFS_FETCH_ENDPOINTS)
