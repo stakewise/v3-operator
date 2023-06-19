06b156ddfb56d7e37ff648865cdfc68d23ac5e67
antares-sw <23400824+antares-sw@users.noreply.github.com>
2023-06-19 12:46:04 +0300

    Add test

--- src/commands/tests/test_create_keys.py
+++ src/commands/tests/test_create_keys.py
@@ -1,3 +1,4 @@
+import glob
 import json
 import os
 import unittest
@@ -54,3 +55,55 @@ class TestCreateKeys(unittest.TestCase):
                 assert len(f.readline()) == 20
 
             assert len(os.listdir(f'{vault_dir}/keystores')) == count + 1
+
+    def test_per_keystore_password(self):
+        vault = faker.eth_address()
+        count = 5
+        runner = CliRunner()
+
+        with runner.isolated_filesystem():
+            args_init = [
+                '--language',
+                'english',
+                '--no-verify',
+                '--vault',
+                vault,
+                '--network',
+                'goerli',
+            ]
+            init_result = runner.invoke(init, args_init)
+            mnemonic = init_result.output.strip()
+            args = [
+                '--mnemonic',
+                f'"{mnemonic}"',
+                '--count',
+                count,
+                '--vault',
+                vault,
+                '--per-keystore-password',
+            ]
+            result = runner.invoke(create_keys, args)
+            assert result.exit_code == 0
+
+            vault_dir = f'{DATA_DIR}/{vault}'
+
+            output = (
+                'Creating validator keys:\t\t\n'
+                'Generating deposit data JSON\t\t\n'
+                'Exporting validator keystores\t\t\n'
+                f'Done. Generated 5 keys for {vault} vault.\n'
+                f'Keystores saved to {vault_dir}/keystores file\n'
+                f'Deposit data saved to {vault_dir}/deposit_data.json file'
+            )
+            assert output.strip() == result.output.strip()
+            with open(f'{vault_dir}/deposit_data.json', encoding='utf-8') as f:
+                data = json.load(f)
+                assert count == len(data)
+                assert data[0].get('network_name') == 'goerli'
+                assert data[0].get('fork_version') == '00001020'
+                assert data[0].get('deposit_cli_version') == src.__version__
+            password_files = glob.glob(os.path.join(vault_dir, '/keystores/*.txt'))
+            for password_file in password_files:
+                with open(password_file, 'r', encoding='utf-8') as f:
+                    assert len(f.readline()) == 20
+            assert len(os.listdir(f'{vault_dir}/keystores')) == count * 2
