9625d866d6350aba2dca1e59c6d601e889e8c719
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2023-07-06 13:53:28 +0300

    Refactor create_wallet command

--- src/commands/create_wallet.py
+++ src/commands/create_wallet.py
@@ -6,12 +6,19 @@ import click
 from eth_account import Account
 from eth_typing import ChecksumAddress, HexAddress
 
+from src.common.config import VaultConfig
 from src.common.contrib import greenify
 from src.common.password import get_or_create_password_file
 from src.common.validators import validate_eth_address, validate_mnemonic
-from src.config.settings import DATA_DIR
 
 
+@click.option(
+    '--data-dir',
+    default=str(Path.home() / '.stakewise'),
+    envvar='SW_DATA_DIR',
+    help='Path where the vault data will be placed. Defaults to ~/.stakewise.',
+    type=click.Path(exists=True, file_okay=False, dir_okay=True),
+)
 @click.option(
     '--mnemonic',
     help='The mnemonic for generating the wallet.',
@@ -21,21 +28,17 @@ from src.config.settings import DATA_DIR
 )
 @click.option(
     '--vault',
-    '--withdrawal-address',
-    help='The withdrawal address where the funds will be sent after validators withdrawals.',
-    prompt='Enter the Vault address',
+    help='The address of the vault.',
+    prompt='Enter the vault address',
     type=str,
     callback=validate_eth_address,
 )
-@click.option(
-    '--data-dir',
-    required=False,
-    help='Path where the vault data will be placed. ' 'Defaults to ~/.stakewise/<vault>',
-    type=click.Path(exists=False, file_okay=False, dir_okay=True),
-)
 @click.command(help='Creates the encrypted hot wallet from the mnemonic.')
 def create_wallet(mnemonic: str, vault: HexAddress, data_dir: str) -> None:
-    wallet_dir = Path(f'{data_dir or DATA_DIR}/{vault.lower()}/wallet')
+    vault_config = VaultConfig(vault, Path(data_dir))
+    vault_config.load(mnemonic)
+    wallet_dir = vault_config.vault_dir / 'wallet'
+
     wallet_dir.mkdir(parents=True, exist_ok=True)
     address = _generate_encrypted_wallet(mnemonic, str(wallet_dir))
     click.echo(
