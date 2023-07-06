57ba0eaff93f3692ef90401ad4ba4137f4e7367e
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2023-07-06 16:51:57 +0300

    Fix create wallet test

--- src/commands/tests/test_create_wallet.py
+++ src/commands/tests/test_create_wallet.py
@@ -3,11 +3,10 @@ import unittest
 
 from click.testing import CliRunner
 from eth_account import Account
-from staking_deposit.key_handling.key_derivation.mnemonic import get_mnemonic
 from sw_utils.tests.factories import faker
 
 from src.commands.create_wallet import create_wallet
-from src.common.language import WORD_LISTS_PATH
+from src.commands.init import init
 from src.config.settings import DATA_DIR
 
 
@@ -16,12 +15,21 @@ class TestCreateWallet(unittest.TestCase):
         vault = faker.eth_address()
         vault_dir = f'{DATA_DIR}/{vault.lower()}'
         runner = CliRunner()
-        Account.enable_unaudited_hdwallet_features()
-        mnemonic = get_mnemonic(language='english', words_path=WORD_LISTS_PATH)
-        account = Account().from_mnemonic(mnemonic=mnemonic)
-        args = ['--mnemonic', f'"{mnemonic}"', '--vault', vault]
         with runner.isolated_filesystem():
-            result = runner.invoke(create_wallet, args)
+            args_init = [
+                '--language',
+                'english',
+                '--no-verify',
+                '--vault',
+                vault,
+                '--network',
+                'goerli',
+            ]
+            init_result = runner.invoke(init, args_init)
+            mnemonic = init_result.output.strip()
+            Account.enable_unaudited_hdwallet_features()
+            account = Account().from_mnemonic(mnemonic=mnemonic)
+            result = runner.invoke(create_wallet, ['--mnemonic', f'"{mnemonic}"', '--vault', vault])
             assert result.exit_code == 0
             filename = 'wallet.json'
             output = 'Done. The wallet and password saved to'
