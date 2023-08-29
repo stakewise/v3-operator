4d7f39f4cfbc6fbc9055dd99810a926dd794da65
antares-sw <23400824+antares-sw@users.noreply.github.com>
2023-08-29 13:27:56 +0300

    Merge key-manager to operator (#152)
    * Merge key-manager to operator
    
    * Add PG Deps
    
    * Merge commands
    
    * Fix
    
    * Move tests
    
    * Move tests
    
    ---------
    
    Signed-off-by: antares-sw <23400824+antares-sw@users.noreply.github.com>

--- src/common/contrib.py
+++ src/common/contrib.py
@@ -1,3 +1,6 @@
+import collections
+from base64 import b64decode, b64encode
+
 import click
 
 
@@ -8,3 +11,15 @@ def chunkify(items, size):
 
 def greenify(value):
     return click.style(value, bold=True, fg='green')
+
+
+def bytes_to_str(value: bytes) -> str:
+    return b64encode(value).decode('ascii')
+
+
+def str_to_bytes(value: str) -> bytes:
+    return b64decode(value)
+
+
+def is_lists_equal(x: list, y: list) -> bool:
+    return collections.Counter(x) == collections.Counter(y)
