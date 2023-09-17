c83aff143a8b2cdf16bc2460439d639fdf38e85f
cyc60 <avsysoev60@gmail.com>
2023-09-17 23:42:34 +0300

    Get available validator by deposit data index
    Signed-off-by: cyc60 <avsysoev60@gmail.com>

--- src/validators/execution.py
+++ src/validators/execution.py
@@ -172,8 +172,10 @@ async def get_available_validators(
     if len(deposit_data.validators) < count:
         return []
 
-    for i in range(start_index, start_index + count):
-        validator = deposit_data.validators[i]
+    validators_range = range(start_index, start_index + count)
+    for validator in deposit_data.validators:
+        if validator.deposit_data_index not in validators_range:
+            continue
         if validator.public_key not in keystores:
             logger.warning(
                 'Cannot find validator with public key %s in imported keystores.',
@@ -190,7 +192,8 @@ async def get_available_validators(
             break
 
         validators.append(validator)
-
+        if len(validators) == count:
+            break
     return validators
 
 
