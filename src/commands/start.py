c4d83ec56d0c361792940c091437a838bcb0fe55
Evgeny Gusarov <evgeny@stakewise.io>
2023-10-16 18:11:23 +0300

    Decrease sw-utils logging

--- src/commands/start.py
+++ src/commands/start.py
@@ -372,6 +372,11 @@ def setup_logging():
         datefmt='%Y-%m-%d %H:%M:%S',
         level=settings.log_level,
     )
+    if not settings.verbose:
+        logging.getLogger('sw_utils.execution').setLevel(logging.ERROR)
+        logging.getLogger('sw_utils.consensus').setLevel(logging.ERROR)
+        logging.getLogger('sw_utils.ipfs').setLevel(logging.ERROR)
+        logging.getLogger('sw_utils.decorators').setLevel(logging.ERROR)
 
 
 async def get_chain_finalized_head() -> ChainHead:
