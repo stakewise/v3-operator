f7fd19eb37ae1c933348b948e9f6a0dbd5ed0d64
Alexander Sysoev <avsysoev60@gmail.com>
2023-11-23 17:26:23 +0300

    Remote-db: fix keys per validator calculation (#241)
    * Remote-db: fix keys per validator calculation
    
    Signed-off-by: cyc60 <avsysoev60@gmail.com>
    
    * Review fix
    
    Signed-off-by: cyc60 <avsysoev60@gmail.com>
    
    ---------
    
    Signed-off-by: cyc60 <avsysoev60@gmail.com>

--- src/remote_db/tests/test_tasks.py
+++ src/remote_db/tests/test_tasks.py
@@ -15,7 +15,7 @@ from web3 import Web3
 from src.common.typings import Oracles
 from src.remote_db.commands import remote_db_group
 from src.remote_db.database import ConfigsCrud, KeyPairsCrud
-from src.remote_db.tasks import _encrypt_private_key
+from src.remote_db.tasks import _encrypt_private_key, _get_key_indexes
 from src.remote_db.typings import RemoteDatabaseKeyPair
 from src.validators.signing.key_shares import private_key_to_private_key_shares
 from src.validators.typings import BLSPrivkey
@@ -395,3 +395,29 @@ class TestRemoteDbSetupOperator:
             result = runner.invoke(remote_db_group, args)
             output = 'Successfully created operator configuration file.\n'
             assert output.strip() in result.output.strip()
+
+
+def test_get_key_indexes():
+    assert _get_key_indexes(1, 1, 0) == (0, 1)
+
+    assert _get_key_indexes(2, 1, 0) == (0, 2)
+
+    assert _get_key_indexes(2, 2, 0) == (0, 1)
+    assert _get_key_indexes(2, 2, 1) == (1, 2)
+
+    assert _get_key_indexes(27, 2, 0) == (0, 14)
+    assert _get_key_indexes(27, 2, 1) == (14, 27)
+
+    assert _get_key_indexes(150, 3, 0) == (0, 50)
+    assert _get_key_indexes(150, 3, 1) == (50, 100)
+    assert _get_key_indexes(150, 3, 2) == (100, 150)
+
+    total = 50
+    count = 0
+    for i in range(total):
+        if i == 49:
+            assert _get_key_indexes(199, total, i) == (196, 199)
+        else:
+            assert _get_key_indexes(199, total, i) == (i * 4, i * 4 + 4)
+        count += _get_key_indexes(199, total, i)[1] - _get_key_indexes(199, total, i)[0]
+    assert count == 199
