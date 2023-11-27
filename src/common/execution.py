5d13e32a4ec28f50438a9907c7dedd4167b63f1e
Evgeny Gusarov <evgeny@stakewise.io>
2023-11-27 19:11:46 +0300

    Rework gas estimate

--- src/common/execution.py
+++ src/common/execution.py
@@ -1,9 +1,7 @@
 import logging
-import statistics
 
 from web3 import Web3
-from web3.exceptions import MethodUnavailable
-from web3.types import BlockIdentifier, Wei
+from web3.types import Wei
 
 from src.common.clients import execution_client, ipfs_fetch_client
 from src.common.contracts import keeper_contract
@@ -97,7 +95,8 @@ async def get_oracles() -> Oracles:
 
 
 async def check_gas_price() -> bool:
-    max_fee_per_gas = await _get_max_fee_per_gas()
+    max_fee_per_gas = await execution_client.eth.gas_price
+
     if max_fee_per_gas >= Web3.to_wei(settings.max_fee_per_gas_gwei, 'gwei'):
         logging.warning(
             'Current gas price (%s gwei) is too high. '
@@ -108,30 +107,3 @@ async def check_gas_price() -> bool:
         return False
 
     return True
-
-
-async def _get_max_fee_per_gas() -> Wei:
-    try:
-        priority_fee = await execution_client.eth.max_priority_fee
-    except MethodUnavailable:
-        priority_fee = await _calculate_median_priority_fee()
-    latest_block = await execution_client.eth.get_block('latest')
-    base_fee = latest_block['baseFeePerGas']
-    max_fee_per_gas = priority_fee + 2 * base_fee
-    return Wei(max_fee_per_gas)
-
-
-async def _calculate_median_priority_fee(block_id: BlockIdentifier = 'latest') -> Wei:
-    block = await execution_client.eth.get_block(block_id)
-
-    # collect maxPriorityFeePerGas for all transactions in the block
-    priority_fees = []
-    for tx_hash in block.transactions:  # type: ignore[attr-defined]
-        tx = await execution_client.eth.get_transaction(tx_hash)
-        if 'maxPriorityFeePerGas' in tx:
-            priority_fees.append(tx.maxPriorityFeePerGas)  # type: ignore[attr-defined]
-
-    if not priority_fees:
-        return await _calculate_median_priority_fee(block['number'] - 1)
-
-    return Wei(statistics.median(priority_fees))
