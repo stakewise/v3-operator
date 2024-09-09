d5944b8c062bcbb8a49cb973bb94e010bd988da2
evgeny-stakewise <123374581+evgeny-stakewise@users.noreply.github.com>
2024-09-09 15:15:28 +0300

    Bump version v2.1.2 (#411)

--- scripts/install.sh
+++ scripts/install.sh
@@ -281,7 +281,7 @@ http_copy() {
 github_release() {
   owner_repo=$1
   version=$2
-  test -z "$version" && version="v2.1.1"
+  test -z "$version" && version="v2.1.2"
   giturl="https://github.com/${owner_repo}/releases/${version}"
   json=$(http_copy "$giturl" "Accept:application/json")
   test -z "$json" && return 1
