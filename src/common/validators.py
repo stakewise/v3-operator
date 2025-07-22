a41faa769bac914ea1518942a2348889f23ab871
evgeny-stakewise <123374581+evgeny-stakewise@users.noreply.github.com>
2025-07-22 22:27:42 +0300

    Add type hints for validators (#491)

--- src/common/validators.py
+++ src/common/validators.py
@@ -1,19 +1,21 @@
+# pylint: disable=unused-argument
 import re
 
 import click
+from eth_typing import ChecksumAddress
 from eth_utils import is_address, to_checksum_address
 
 from src.common.language import validate_mnemonic as verify_mnemonic
 
 
-# pylint: disable-next=unused-argument
-def validate_mnemonic(ctx, param, value):  # type: ignore
+def validate_mnemonic(ctx: click.Context, param: click.Parameter, value: str) -> str:
     value = value.replace('"', '')
     return verify_mnemonic(value)
 
 
-# pylint: disable-next=unused-argument
-def validate_eth_address(ctx, param, value):  # type: ignore
+def validate_eth_address(
+    ctx: click.Context, param: click.Parameter, value: str | None
+) -> ChecksumAddress | None:
     if not value:
         return None
     try:
@@ -25,15 +27,16 @@ def validate_eth_address(ctx, param, value):  # type: ignore
     raise click.BadParameter('Invalid Ethereum address')
 
 
-# pylint: disable-next=unused-argument
-def validate_db_uri(ctx, param, value):  # type: ignore
+def validate_db_uri(ctx: click.Context, param: click.Parameter, value: str) -> str:
     pattern = re.compile(r'.+:\/\/.+:.*@.+\/.+')
     if not pattern.match(value):
         raise click.BadParameter('Invalid database connection string')
     return value
 
 
-def validate_dappnode_execution_endpoints(ctx, param, value):  # type: ignore
+def validate_dappnode_execution_endpoints(
+    ctx: click.Context, param: click.Parameter, value: str
+) -> str | None:
     dappnode = ctx.params.get('dappnode')
     if dappnode and not value:
         raise click.MissingParameter(
