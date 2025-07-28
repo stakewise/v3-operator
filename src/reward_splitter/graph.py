2e45be1d53244d5662f8233f130f50d474b11a5e
cyc60 <avsysoev60@gmail.com>
2025-07-28 11:39:53 +0300

    Fix reward splitter query
    Signed-off-by: cyc60 <avsysoev60@gmail.com>

--- src/reward_splitter/graph.py
+++ src/reward_splitter/graph.py
@@ -18,7 +18,7 @@ async def graph_get_reward_splitters(
 ) -> list[RewardSplitter]:
     query = gql(
         '''
-        query Query($block: Int, $first: Int, $skip: Int, $claimer: String, $vaults: [String]) {
+        query Query($block: Int, $first: Int, $skip: Int, $claimer: Bytes, $vaults: [String]) {
             rewardSplitters(
                 block: {number: $block},
                 where: {
