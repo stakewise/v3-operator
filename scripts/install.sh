caa61168a4c86c3cc92c433e6e1ded4a5514e3e5
Alexander Sysoev <avsysoev60@gmail.com>
2025-10-07 15:39:04 +0300

    Fix ubuntu binary (#573)
    * Fix ubuntu binary
    
    Signed-off-by: cyc60 <avsysoev60@gmail.com>
    
    * Add ubuntu 22.04 build
    
    Signed-off-by: cyc60 <avsysoev60@gmail.com>
    
    * Update release paths
    
    Signed-off-by: cyc60 <avsysoev60@gmail.com>
    
    * Add 22.04 arm
    
    Signed-off-by: cyc60 <avsysoev60@gmail.com>
    
    ---------
    
    Signed-off-by: cyc60 <avsysoev60@gmail.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -281,7 +281,7 @@ http_copy() {
 github_release() {
   owner_repo=$1
   version=$2
-  test -z "$version" && version="v4.0.2"
+  test -z "$version" && version="v4.0.3"
   giturl="https://github.com/${owner_repo}/releases/${version}"
   json=$(http_copy "$giturl" "Accept:application/json")
   test -z "$json" && return 1
