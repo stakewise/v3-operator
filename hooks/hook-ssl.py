9ac985f68435f7eb3bb0af48e2c346ad9cdd3608
Evgeny Gusarov <evgeny@stakewise.io>
2026-02-11 18:37:56 +0300

    cleanup main.py

--- hooks/hook-ssl.py
+++ hooks/hook-ssl.py
@@ -4,6 +4,7 @@ PyInstaller runtime hook that configures SSL certificates.
 The bundled Python cannot access macOS/system CA certificates,
 so we point SSL_CERT_FILE to the certifi CA bundle included in the binary.
 """
+
 import os
 import sys
 
