61ea30256c9d6764dc5babdb997029b8631d2bc5
Alexander Sysoev <avsysoev60@gmail.com>
2026-05-28 22:09:31 +0300

    Allow funding for unregistered consensus validators (#735)
    * Allow funding for new validators
    
    Signed-off-by: cyc60 <avsysoev60@gmail.com>
    
    * Improve naming
    
    Signed-off-by: cyc60 <avsysoev60@gmail.com>
    
    * Refactored fetch_compounding_validators_balances
    
    Signed-off-by: cyc60 <avsysoev60@gmail.com>
    
    ---------
    
    Signed-off-by: cyc60 <avsysoev60@gmail.com>

--- src/validators/consensus.py
+++ src/validators/consensus.py
@@ -1,4 +1,5 @@
 import logging
+from collections import defaultdict
 from itertools import batched
 
 from eth_typing import HexStr
@@ -29,20 +30,30 @@ async def fetch_compounding_validators_balances() -> dict[HexStr, Gwei]:
     if not vault_public_keys:
         return {}
 
+    # Initialize balances for every validator
+    # including those that are not present in CL yet, but have pending deposits.
+    validators_balances = defaultdict(lambda: Gwei(0), {key: Gwei(0) for key in vault_public_keys})
+
+    # Fetch consensus validators
     consensus_block = await consensus_client.get_block('head')
     slot = consensus_block['data']['message']['slot']
     consensus_validators = await fetch_consensus_validators(list(vault_public_keys), slot=slot)
 
-    validators_balances = {
-        v.public_key: v.balance
-        for v in consensus_validators
-        if v.is_compounding and v.status not in EXITING_STATUSES
-    }
+    # Filter compounding and remove exiting/withdrawn validators,
+    # as they are not eligible for funding
+    for validator in consensus_validators:
+        if validator.is_compounding and validator.status not in EXITING_STATUSES:
+            validators_balances[validator.public_key] = validator.balance
+        else:
+            validators_balances.pop(validator.public_key, None)
 
+    # Add balances from pending deposits that are not yet reflected in the consensus node
     all_pending_deposits = await consensus_client.get_pending_deposits(slot)
     for deposit in all_pending_deposits:
         public_key, amount = deposit['pubkey'], int(deposit['amount'])
         if public_key not in validators_balances:
+            continue
+        if not deposit['withdrawal_credentials'].startswith('0x02'):
             continue
         validators_balances[public_key] = Gwei(validators_balances[public_key] + amount)
 
