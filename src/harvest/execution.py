950715973a46de76a9b1a085f0e2a3e554b97a9e
evgeny-stakewise <123374581+evgeny-stakewise@users.noreply.github.com>
2026-07-22 15:05:57 +0300

    Fix outdated web3 exception handling (#771)
    * Catch ContractLogicError instead of legacy ValueError
    
    * Call updateState directly instead of via multicall
    
    * Catch Web3Exception in harvest

--- src/harvest/execution.py
+++ src/harvest/execution.py
@@ -2,9 +2,9 @@ import logging
 
 from eth_typing import HexStr
 from web3 import Web3
-from web3.exceptions import ContractLogicError
+from web3.exceptions import Web3Exception
 
-from src.common.contracts import VaultContract, multicall_contract
+from src.common.contracts import VaultContract
 from src.common.transaction import tx_manager
 from src.common.typings import HarvestParams
 from src.common.utils import format_error
@@ -15,13 +15,17 @@ logger = logging.getLogger(__name__)
 
 async def submit_harvest_transaction(harvest_params: HarvestParams) -> HexStr | None:
     vault_contract = VaultContract(settings.vault)
-    calls = [
-        (vault_contract.contract_address, vault_contract.get_update_state_call(harvest_params))
-    ]
     try:
-        tx_function = multicall_contract.functions.aggregate(calls)
+        tx_function = vault_contract.functions.updateState(
+            (
+                harvest_params.rewards_root,
+                harvest_params.reward,
+                harvest_params.unlocked_mev_reward,
+                harvest_params.proof,
+            )
+        )
         tx_receipt = await tx_manager.transact(tx_function)
-    except (ValueError, ContractLogicError) as e:
+    except Web3Exception as e:
         logger.error('Failed to harvest: %s', format_error(e))
         if settings.verbose:
             logger.exception(e)
