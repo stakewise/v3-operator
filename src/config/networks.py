c558be2ef7a9f18eb9351709079ec7f6cbaa69b0
evgeny-stakewise <123374581+evgeny-stakewise@users.noreply.github.com>
2026-09-22 11:11:01 +0300

    Remove unreachable genesis block event scan in ProcessedSharesCache (#848)

--- src/config/networks.py
+++ src/config/networks.py
@@ -41,7 +41,6 @@ class NetworkConfig(BaseNetworkConfig):
     STAKEWISE_GRAPH_ENDPOINT: str
     RATED_API_URL: str
     CHECKPOINTS: CheckpointsConfig
-    OS_TOKEN_REDEEMER_GENESIS_BLOCK: BlockNumber
     MAX_FEE_PER_GAS_GWEI: Gwei
     MAX_VALIDATOR_BALANCE_GWEI: Gwei
     SHARD_COMMITTEE_PERIOD: int
@@ -141,7 +140,6 @@ NETWORKS: dict[str, NetworkConfig] = {
             EXIT_SIGNATURES_LAST_EVENT_BLOCK=None,
             EXIT_SIGNATURES_CHECKPOINT_BLOCK=BlockNumber(25934000),
         ),
-        OS_TOKEN_REDEEMER_GENESIS_BLOCK=BlockNumber(24923158),
         MAX_FEE_PER_GAS_GWEI=Gwei(10),
         MAX_VALIDATOR_BALANCE_GWEI=Gwei(int(Web3.from_wei(Web3.to_wei(300, 'ether'), 'gwei'))),
         SHARD_COMMITTEE_PERIOD=256,  # epochs
@@ -201,7 +199,6 @@ NETWORKS: dict[str, NetworkConfig] = {
             EXIT_SIGNATURES_LAST_EVENT_BLOCK=None,
             EXIT_SIGNATURES_CHECKPOINT_BLOCK=BlockNumber(3584000),
         ),
-        OS_TOKEN_REDEEMER_GENESIS_BLOCK=BlockNumber(2657589),
         MAX_FEE_PER_GAS_GWEI=Gwei(10),
         MAX_VALIDATOR_BALANCE_GWEI=Gwei(int(Web3.from_wei(Web3.to_wei(300, 'ether'), 'gwei'))),
         SHARD_COMMITTEE_PERIOD=256,  # epochs
@@ -263,7 +260,6 @@ NETWORKS: dict[str, NetworkConfig] = {
             EXIT_SIGNATURES_LAST_EVENT_BLOCK=None,
             EXIT_SIGNATURES_CHECKPOINT_BLOCK=BlockNumber(48148000),
         ),
-        OS_TOKEN_REDEEMER_GENESIS_BLOCK=BlockNumber(45773287),
         MAX_FEE_PER_GAS_GWEI=Gwei(2),
         MAX_VALIDATOR_BALANCE_GWEI=Gwei(int(Web3.from_wei(Web3.to_wei(1800, 'ether'), 'gwei'))),
         SHARD_COMMITTEE_PERIOD=256,  # epochs
